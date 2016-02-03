225bc230fb623766a45f82aeaed71f68fa259e44
Ian Juma <ian.juma@yahoo.com>
2016-02-03 12:39:44 +0300

    building a gem

---
+++ lib/africastalking/ruby.rb
@@ -0,0 +1,7 @@
+require "africastalking/ruby/version"
+
+module Africastalking
+  module Ruby
+    # Your code goes here...
+  end
+end
---
+++ lib/africastalking/ruby/version.rb
@@ -0,0 +1,5 @@
+module Africastalking
+  module Ruby
+    VERSION = "0.1.0"
+  end
+end
